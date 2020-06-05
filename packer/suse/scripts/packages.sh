f4eb986cd7ae1123de8bd79165f5ff78bf612930
Henryk Paluch <henryk.paluch@pickering.cz>
2020-06-05 12:23:04 +0200

    Removed more crap

--- packer/suse/scripts/packages.sh
+++ packer/suse/scripts/packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-zypper remove -y samba-libs libvorbis gstreamer os-prober libX11-data
+zypper remove -y samba-libs libvorbis gstreamer os-prober libX11-data \
+	ypbind yp-tools cpio-mt crda tcl cantarell-fonts open-iscsi \
+	cups-client cups-config wireless-regdb xfsprogs \
+	libbtrfs0 btrfsmaintenance btrfsprogs \
+	btrfsprogs-udev-rules poppler-data kernel-firmware
 zypper install -y mc curl net-tools-deprecated
 
 
