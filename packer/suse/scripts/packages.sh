6a7cf1fe9b5bb22babc68cfc18a91d7fda43a45c
Henryk Paluch <henryk.paluch@pickering.cz>
2021-04-21 11:46:32 +0200

    Do not install recommended

--- packer/suse/scripts/packages.sh
+++ packer/suse/scripts/packages.sh
@@ -5,6 +5,6 @@ zypper remove -y samba-libs libvorbis gstreamer os-prober libX11-data \
 	cups-client cups-config wireless-regdb xfsprogs \
 	libbtrfs0 btrfsmaintenance btrfsprogs \
 	btrfsprogs-udev-rules poppler-data kernel-firmware
-zypper install -y mc curl net-tools-deprecated
+zypper install --no-recommends -y mc curl net-tools-deprecated
 
 
